f199bfd7e60b22c142867d2f6185e680c59dfcd1
lissdx <yuri.ageloc@gmail.com>
2015-04-18 14:44:46 -0400

    Proxy Pattern added

---
+++ ProxyDesignPattern/src/GumballMachine.java
@@ -0,0 +1,14 @@
+
+public class GumballMachine {
+    private final String location;
+    
+    public GumballMachine( final String location, int count ) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+    
+    
+}
---
+++ ProxyDesignPattern/src/GumballMonitor.java
@@ -0,0 +1,14 @@
+
+public class GumballMonitor {
+    private final GumballMachine gumballMachine;
+
+    public GumballMonitor( final GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+    
+    public GumballMachine getGumballMachine() {
+        return gumballMachine;
+    }
+    
+    
+}
